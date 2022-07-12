bb0dd7138ec801d0da8b508f5a67b7c8767f09a2
paksin <104784027+paksin@users.noreply.github.com>
2022-07-12 00:37:49 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ else if (randomNumber2<randomNumber1){
 else{
   document.querySelector(".result").innerHTML = ("Draw!");
 }
+
+document.querySelector(".img1").addEventListener("click", function() {
+  randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 +".png");
+  if (randomNumber2>randomNumber1){
+    document.querySelector(".result").innerHTML = ("Player 2 Wins!");
+  }
+  else if (randomNumber2<randomNumber1){
+    document.querySelector(".result").innerHTML = ("Player 1 Wins!");
+  }
+  else{
+    document.querySelector(".result").innerHTML = ("Draw!");
+  }
+});
+
+document.querySelector(".img2").addEventListener("click", function() {
+  randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 +".png");
+  if (randomNumber2>randomNumber1){
+    document.querySelector(".result").innerHTML = ("Player 2 Wins!");
+  }
+  else if (randomNumber2<randomNumber1){
+    document.querySelector(".result").innerHTML = ("Player 1 Wins!");
+  }
+  else{
+    document.querySelector(".result").innerHTML = ("Draw!");
+  }
+});
